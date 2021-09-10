7883748daf54ce93aa53b90b6c022f7b65386a3c
Luiz Felipe Wendt Campos <luizfelipewendt@gmail.com>
2021-09-10 09:10:59 -0300

    criação do autor

---
+++ casa-do-codigo/src/main/java/com/deveficiente/cdc/novoautor/Autor.java
@@ -0,0 +1,43 @@
+package com.deveficiente.cdc.novoautor;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Entity
+public class Autor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+    private String descricao;
+    private LocalDateTime instanteCriacao = LocalDateTime.now();
+
+    public Autor(@NotBlank String nome,
+                 @NotBlank @Email String email,
+                 @NotBlank @Size(max = 400) String descricao) {
+        this.nome = nome;
+        this.email = email;
+        this.descricao = descricao;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Autor{");
+        sb.append("id=").append(id);
+        sb.append(", nome='").append(nome).append('\'');
+        sb.append(", email='").append(email).append('\'');
+        sb.append(", descricao='").append(descricao).append('\'');
+        sb.append(", instanteCriacao=").append(instanteCriacao);
+        sb.append('}');
+        return sb.toString();
+    }
+}
---
+++ casa-do-codigo/src/main/java/com/deveficiente/cdc/novoautor/AutoresController.java
@@ -0,0 +1,29 @@
+package com.deveficiente.cdc.novoautor;
+
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.validation.Valid;
+
+//2
+@RestController
+public class AutoresController {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @PostMapping("/autores")
+    @Transactional
+    //1
+    //2
+    public String cria(@RequestBody @Valid NovoAutorRequest request) {
+        //1
+        Autor autor = request.toModel();
+        manager.persist(autor);
+        return autor.toString();
+    }
+}
