02a83a67d4965bd5823a8655cc9d7bddac8554aa
luizfelipew <luizfelipewendt@gmail.com>
2024-09-02 17:13:21 -0300

    testando a classe Cupom

---
+++ casa-do-codigo/src/test/java/com/deveficiente/cdc/cadastrocupom/CupomTest.java
@@ -0,0 +1,44 @@
+package com.deveficiente.cdc.cadastrocupom;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CupomTest {
+
+
+    @ParameterizedTest
+    @CsvSource({
+            "0,true",
+            "1,true"
+    })
+    void teste1(long valor,boolean resultado) throws Exception {
+        Cupom cupom = new Cupom("codigo", BigDecimal.TEN, LocalDate.now().plusDays(valor));
+        Assertions.assertEquals(resultado, cupom.valido());
+    }
+
+    @Test
+    @DisplayName("cupom com data passada nao eh mais valido")
+    void teste2() throws Exception {
+        Cupom cupom = new Cupom("codigo", BigDecimal.TEN, LocalDate.now().plusDays(1));
+        ReflectionTestUtils.setField(cupom, "validade", LocalDate.now().minusDays(1));
+        Assertions.assertFalse(cupom.valido());
+    }
+
+    @Test
+    @DisplayName("não pode criar cupom com data no passado")
+    void teste3() throws Exception {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            new Cupom("codigo", BigDecimal.TEN, LocalDate.now().minusDays(1));
+        });
+
+    }
+}
